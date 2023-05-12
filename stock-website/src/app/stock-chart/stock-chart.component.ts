94d0773434be229652d3abaf65fd5925b676c090
Mira Antolovich <48299535+mla4783@users.noreply.github.com>
2023-05-12 11:12:25 -0400

    first & secondary graph done
    API work coming soon

--- stock-website/src/app/stock-chart/stock-chart.component.ts
+++ stock-website/src/app/stock-chart/stock-chart.component.ts
@@ -1,13 +1,20 @@
 // #region Setup
 import { Component, ViewChild } from "@angular/core";
 import { ReactiveFormsModule } from '@angular/forms';
-import { ApexDataLabels, ApexMarkers, ApexYAxis, NgApexchartsModule } from 'ng-apexcharts';
 import {
   ChartComponent,
   ApexAxisChartSeries,
   ApexChart,
+  ApexTitleSubtitle,
   ApexXAxis,
-  ApexTitleSubtitle
+  ApexDataLabels,
+  ApexStroke,
+  ApexYAxis,
+  ApexMarkers,
+  ApexAnnotations,
+  ApexFill,
+  ApexForecastDataPoints,
+  ApexLegend
 } from "ng-apexcharts";
 
 export type ChartOptions = {
@@ -16,8 +23,10 @@ export type ChartOptions = {
   xaxis: ApexXAxis;
   yaxis: ApexYAxis;
   title: ApexTitleSubtitle;
+  stroke: ApexStroke;
   markers: ApexMarkers;
   dataLabels: ApexDataLabels;
+  annotations: ApexAnnotations;
 };
 
 @Component({
@@ -35,16 +44,16 @@ export class StockChartComponent {
   protected chart2Options!: Partial<ChartOptions> | any;
   protected commonOptions: Partial<ChartOptions> | any;
 
-  protected graphTypes = ["line", "bar", "area"];
+  protected graphTypes = ["line", "candle"];
   // implement candle later based on data
   protected stockTypes = ["AAPL", "GOOG", "MSFT"];
 
   protected selectedGraphType = this.graphTypes[0];
   protected selectedStockType = this.stockTypes[0];
 
-  //protected stock_data = [{x: "Jan", y: 10}, {x: "Feb", y: 41}, {x: "Mar", y: 35}, {x: "Apr", y: 51}, {x: "May", y: 49}, {x: "Jun", y: 62}, 
-  //                        {x: "Jul", y: 69}, {x: "Aug", y: 91}, {x: "Sep", y: 148}];
-  protected stock_data = [10, 41, 35, 51, 49, 62, 69, 91, 148];
+  protected stock_data = [{x: "Jan", y: 10}, {x: "Feb", y: 41}, {x: "Mar", y: 35}, {x: "Apr", y: 51}, {x: "May", y: 49}, {x: "Jun", y: 62}, 
+                          {x: "Jul", y: 69}, {x: "Aug", y: 91}, {x: "Sep", y: 148}];
+  //protected stock_data = [10, 41, 35, 51, 49, 62, 69, 91, 148];
   protected x_axis_data = ["Jan", "Feb",  "Mar",  "Apr",  "May",  "Jun",  "Jul",  "Aug", "Sep"]
   protected advanced_data = [10000, 41000, 35000, 51000, 49000, 62000, 69000, 91000, 148000];
   
@@ -57,7 +66,7 @@ export class StockChartComponent {
   protected showBB = false;
   // #endregion
 
-  
+
   // #region Constructor
   constructor() {  
     this.initCharts()
@@ -74,15 +83,18 @@ export class StockChartComponent {
     };
 
     this.chart1Options = {
-      series: [{
-        data: this.stock_data,
-        name: this.selectedStockType
-      }],
+      series: [
+        {
+          type: "line",
+          name: this.selectedStockType,
+          data: this.stock_data
+        }
+      ],
       chart: {
-        height: 600,
-        id: "main",
-        type: "line",
-        group: "stock"
+          height: 600,
+          id: "main",
+          type: "line",
+          group: "stock"
       },
       xaxis: {
         categories: this.x_axis_data
@@ -110,7 +122,7 @@ export class StockChartComponent {
         id: "advanced",
         group: "stock",
         type: "area",
-        height: 200
+        height: 250
       },
       dataLabels: {
         enabled: false
@@ -124,166 +136,546 @@ export class StockChartComponent {
           minWidth: 60,
           maxWidth: 60
         }
+      },
+      stroke: {
+        curve: "straight",
+        width: [4, 4, 4, 4, 4, 4],
       }
     };
   }
   // #endregion
 
+
   // Would it be better for the API to give me the entire data-set for the stock & then I pick and choose what data to display 
   // or would it be better for my API to call only the data I need and then I format it accordingly?
-  protected changeChart1() {
-    var data_chart1 = []
-    
-    // line & candle
+  protected changeChart1() {    
+    // line
     if (this.selectedGraphType == "line") {
-      console.log("line")
-      data_chart1.push({
-        data: this.stock_data,
-        name: this.selectedStockType
-      })
+      this.ifLine();
+    }
+    // candle
+    else if (this.selectedGraphType == "candle") {
+      this.ifCandle();
     }
     else {
-      console.log("candle")
-      data_chart1.push(this.stock_data)
+      console.log("This shouldn't be possible! Error!");  
     }
+  }
+  
 
+  private ifLine() {
+    var data_line = []
+    // get stock data just close
+    console.log("line")
+    data_line.push({
+      data: this.stock_data,
+      name: this.selectedStockType
+    })
+
+    // get options 
     if (this.showSMA) {
-      data_chart1.push({
+      data_line.push({
         name: "SMA",
-        data: [10, 25.8333, 23.8889, 32.5185, 37.0123, 45.3372, 52.1124, 70.7408, 104.4939],
+        data: [{x: "Jan", y: 10}, {x: "Feb", y: 30}, {x: "Mar", y: 20}, {x: "Apr", y: 30}, 
+               {x: "May", y: 20}, {x: "Jun", y: 45}, {x: "Jul", y: 68}, {x: "Aug", y: 57}, {x: "Sep", y: 78}],
         type: "line"
       })
     }
 
     if (this.showEMA) {
-      data_chart1.push({
+      data_line.push({
         name: "EMA",
-        data: [10, 25.8333, 23.8889, 32.5185, 37.0123, 45.3372, 52.1124, 70.7408, 104.4939],
+        data: [{x: "Jan", y: 10}, {x: "Feb", y: 25.8333}, {x: "Mar", y: 23.8889}, {x: "Apr", y: 32.5185}, 
+               {x: "May", y: 37.0123}, {x: "Jun", y: 45.3372}, {x: "Jul", y: 52.1124}, {x: "Aug", y: 70.7408}, {x: "Sep", y: 104.4939}],
         type: "line"
       })
     }
 
     if (this.showBB) {
-      data_chart1.push({
-        name: "Lower BB",
-        data: [5, 20.8333, 18.8889, 27.5185, 32.0123, 40.3372, 47.1124, 65.7408, 99.4939],
-        type: "line"
+      data_line.push({   
+          type: "rangeArea",
+          name: "BB",
+          data: [
+            {x: "Jan", y: [4, 5]},
+            {x: "Feb", y: [20, 25]},
+            {x: "Mar", y: [3, 10]},
+            {x: "Apr", y: [8, 16]},
+            {x: "May", y: [13, 22]},
+            {x: "Jun", y: [18, 26]},
+            {x: "Jul", y: [21, 29]},
+            {x: "Aug", y: [21, 28]},
+            {x: "Sep", y: [17, 24]}
+          ]
       })
-
-      data_chart1.push({
-        name: "Upper BB",
-        data: [20, 35.8333, 33.8889, 42.5185, 47.0123, 55.3372, 62.1124, 80.7408, 114.4939],
-        type: "line"
-      })
-
-      // https://apexcharts.com/angular-chart-demos/range-area-charts/combo/ why this no work
-      /*
-      data_chart1.push({   
-        type: "rangeArea",       
-        name: "BB",
-        data: [
-          {y: [-2, 4]},
-          {x: "Feb", y: [-1, 6]},
-          {x: "Mar", y: [3, 10]},
-          {x: "Apr", y: [8, 16]},
-          {x: "May", y: [13, 22]},
-          {x: "Jun", y: [18, 26]},
-          {x: "Jul", y: [21, 29]},
-          {x: "Aug", y: [21, 28]},
-          {x: "Sep", y: [17, 24]}
-        ]
-      })
-      */
     }
 
 
-    console.log(data_chart1);
+    // BB can be rangeArea (change chart to rangeArea, y axis tooltip enabled false)
+    if (this.showBB) {
+      this.chart1Options = {
+        series: data_line,
+        chart: {
+          height: 600,
+          id: "main",
+          type: "rangeArea",
+          group: "stock"
+        },
+        xaxis: {
+          categories: this.x_axis_data
+        },
+        yaxis: {
+          forceNiceScale: true,
+          labels: {
+            minWidth: 60,
+            maxWidth: 60
+          },
+          tooltip: {
+            enabled: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: "straight",
+          width: [4, 4, 4, 4, 4, 4]
+        }
+      }; 
+    }   
+    // Otherwise chart is line (change chart to line,  y axis tooltip enabled false)
+    else {
+      this.chart1Options = {
+        series: data_line,
+        chart: {
+          height: 600,
+          id: "main",
+          type: "line",
+          group: "stock"
+        },
+        xaxis: {
+          categories: this.x_axis_data
+        },
+        yaxis: {
+          forceNiceScale: true,
+          labels: {
+            minWidth: 60,
+            maxWidth: 60
+          },
+          tooltip: {
+            enabled: false
+          }
+        },
+        dataLabels: {
+          enabled: false
+        },
+        stroke: {
+          curve: "straight",
+          width: [4, 4, 4, 4, 4, 4]
+        }
+      }; 
+    }
 
-    //TODO: FIGURE OUT WHY RANGE AREA CHART IS NOT WORKING??
 
-    this.chart1Options.series = data_chart1;  
-
-    //this.chart1Options.series = data_chart1;
   }
-  
+
+  private ifCandle() {
+    var data_candle = []
+    // get stock data open, high, low, close
+    console.log("candle")
+    data_candle.push({
+      type: "candlestick",
+      data: [
+        {x: "Jan", y: [4, 5, 3, 4]},
+        {x: "Feb", y: [20, 25, 10, 14]},
+        {x: "Mar", y: [3, 10, 4, 5]},
+        {x: "Apr", y: [8, 16, 6, 10]},
+        {x: "May", y: [13, 22, 14, 20]},
+        {x: "Jun", y: [18, 26, 19, 21]},
+        {x: "Jul", y: [21, 29, 21, 24]},
+        {x: "Aug", y: [21, 28, 18, 18]},
+        {x: "Sep", y: [17, 24, 19, 23]}
+      ],
+      name: this.selectedStockType
+    })
+
+    // get options 
+    if (this.showSMA) {
+      data_candle.push({
+        name: "SMA",
+        data: [{x: "Jan", y: 10}, {x: "Feb", y: 30}, {x: "Mar", y: 20}, {x: "Apr", y: 30}, 
+               {x: "May", y: 20}, {x: "Jun", y: 45}, {x: "Jul", y: 68}, {x: "Aug", y: 57}, {x: "Sep", y: 78}],
+        type: "line",
+      })
+    }
+
+    if (this.showEMA) {
+      data_candle.push({
+        name: "EMA",
+        data: [{x: "Jan", y: 10}, {x: "Feb", y: 25.8333}, {x: "Mar", y: 23.8889}, {x: "Apr", y: 32.5185}, 
+               {x: "May", y: 37.0123}, {x: "Jun", y: 45.3372}, {x: "Jul", y: 52.1124}, {x: "Aug", y: 70.7408}, {x: "Sep", y: 104.4939}],
+        type: "line"
+      })
+    }
+
+    // BB has to be two lines
+    if (this.showBB) {
+      data_candle.push({   
+          name: "Lower BB",
+          data: [
+            {x: "Jan", y: 4},
+            {x: "Feb", y: 20},
+            {x: "Mar", y: 3},
+            {x: "Apr", y: 8},
+            {x: "May", y: 13},
+            {x: "Jun", y: 18},
+            {x: "Jul", y: 21},
+            {x: "Aug", y: 21},
+            {x: "Sep", y: 17}
+          ],
+          type: "line",
+      })
+      
+      data_candle.push({   
+        name: "Upper BB",
+        data: [
+          {x: "Jan", y: 5},
+          {x: "Feb", y: 25},
+          {x: "Mar", y: 10},
+          {x: "Apr", y: 16},
+          {x: "May", y: 22},
+          {x: "Jun", y: 26},
+          {x: "Jul", y: 29},
+          {x: "Aug", y: 28},
+          {x: "Sep", y: 24}
+        ],
+        type: "line"
+      })
+
+    }
+
+    // Chart has to be candlestick (change chart to candlestick)
+    // y axis tooltip enabled (true)
+    this.chart1Options = {
+      series: data_candle,
+      chart: {
+        height: 600,
+        id: "main",
+        type: "candlestick",
+        group: "stock"
+      },
+      xaxis: {
+        categories: this.x_axis_data
+      },
+      yaxis: {
+        forceNiceScale: true,
+        labels: {
+          minWidth: 60,
+          maxWidth: 60
+        },
+        tooltip: {
+          enabled: true
+        }
+      },
+      stroke: {
+        curve: "straight",
+        width: [4, 4, 4, 4, 4, 4],
+      }
+    }; 
+  }
+
 
   protected changeChart2() {
     var data_chart2 = []
 
     if (this.selectedChart2Type == "RSI") {
-      data_chart2.push({
-        name: "RSI",
-        data: [10, 30, 20, 30, 20, 45, 68, 57, 78],
-        type: "line"
-      })
-    }
+      this.chart2Options = {
+        series: [
+          {
+            name: "RSI",
+            data: [10, 30, 20, 30, 20, 45, 68, 57, 78],
+          }
+        ],
+        chart: {
+          toolbar: {
+            show: false,
+          },  
+          id: "advanced",
+          group: "stock",
+          type: "line",
+          height: 250
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: this.x_axis_data
+        },
+        yaxis: {
+          labels: {
+            minWidth: 60,
+            maxWidth: 60
+          },
+          min: 0,
+          max: 100  
+        },
+        stroke: {
+          curve: "straight",
+          width: [4, 4, 4, 4, 4, 4],
+        },
+        annotations: {
+          yaxis: [
+          {
+            y: 30,
+            borderColor: "green",
+            strokeDashArray: 0,
+            borderWidth: 2,
+            label: {
+              text: "Oversold",
+              style: {
+                color: "#fff",
+                background: "green"
+              }
+            }
+          },
+          {
+            y: 70,
+            borderColor: "red",
+            strokeDashArray: 0,
+            borderWidth: 2,
+            label: {
+              text: "Overbought",
+              style: {
+                color: "#fff",
+                background: "red"
+              }
+            }
+          }
+        ]
+        }
+      }
+    } 
 
     else if (this.selectedChart2Type == "%R") {
-      data_chart2.push({
-        name: "%R",
-        data: [10, 30, 20, 30, 20, 45, 68, 57, 78],
-        type: "line"
-      })
+      this.chart2Options = {
+        series: [
+          {
+            name: "%R",
+            data: [-10, -30, -20, -30, -20, -45, -68, -57, -78]
+          }
+        ],
+        chart: {
+          toolbar: {
+            show: false,
+          },  
+          id: "advanced",
+          group: "stock",
+          type: "line",
+          height: 250
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: this.x_axis_data
+        },
+        yaxis: {
+          labels: {
+            minWidth: 60,
+            maxWidth: 60
+          },
+          min: -100,
+          max: 0  
+        },
+        stroke: {
+          curve: "straight",
+          width: [4, 4, 4, 4, 4, 4],
+        },
+        annotations: {
+          yaxis: [
+          {
+            y: -20,
+            borderColor: "red",
+            strokeDashArray: 0,
+            borderWidth: 2,
+            label: {
+              text: "Overbought",
+              style: {
+                color: "#fff",
+                background: "red"
+              }
+            }
+          },
+          {
+            y: -80,
+            borderColor: "green",
+            strokeDashArray: 0,
+            borderWidth: 2,
+            label: {
+              text: "Oversold",
+              style: {
+                color: "#fff",
+                background: "green"
+              }
+            }
+          }
+        ]
+        }
+      }
     }
 
     else if (this.selectedChart2Type == "SO") {
-      data_chart2.push({
-        name: "SO",
-        data: [10, 30, 20, 30, 20, 45, 68, 57, 78],
-        type: "line"
-      })
+      this.chart2Options = {
+        series: [
+          {
+            name: "SO",
+            data: [10, 30, 20, 30, 20, 45, 68, 57, 78],
+          }
+          // add %K & %D
+        ],
+        chart: {
+          toolbar: {
+            show: false,
+          },  
+          id: "advanced",
+          group: "stock",
+          type: "line",
+          height: 250
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: this.x_axis_data
+        },
+        yaxis: {
+          labels: {
+            minWidth: 60,
+            maxWidth: 60
+          },
+          min: 0,
+          max: 100  
+        },
+        stroke: {
+          curve: "straight",
+          width: [4, 4, 4, 4, 4, 4],
+        },
+        annotations: {
+          yaxis: [
+          {
+            y: 20,
+            borderColor: "green",
+            strokeDashArray: 0,
+            borderWidth: 2,
+            label: {
+              text: "Oversold",
+              style: {
+                color: "#fff",
+                background: "green"
+              }
+            }
+          },
+          {
+            y: 80,
+            borderColor: "red",
+            strokeDashArray: 0,
+            borderWidth: 2,
+            label: {
+              text: "Overbought",
+              style: {
+                color: "#fff",
+                background: "red"
+              }
+            }
+          }
+        ]
+        }
+      }
     }
 
-    else if (this.selectedChart2Type == "M") {
-      data_chart2.push({
-        name: "Momentum",
-        data: [10, 30, 20, 30, 20, 45, 68, 57, 78],
-        type: "line"
-      })
+    else if (this.selectedChart2Type == "Momentum") {
+      this.chart2Options = {
+        series: [
+          {
+            name: "Momentum",
+            data: this.advanced_data,
+          }
+        ],
+        chart: {
+          toolbar: {
+            show: false,
+          },  
+          id: "advanced",
+          group: "stock",
+          type: "line",
+          height: 250
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: this.x_axis_data
+        },
+        yaxis: {
+          forceNiceScale: true,
+          labels: {
+            minWidth: 60,
+            maxWidth: 60
+          }
+        },
+        stroke: {
+          curve: "straight",
+          width: [4, 4, 4, 4, 4, 4],
+        }
+      };  
     }
 
     //show volume
     else {
-      data_chart2.push({
-        name: "Volume",
-        data: [10, 30, 20, 30, 20, 45, 68, 57, 78],
-        type: "area"
-      })
+      this.chart2Options = {
+        series: [
+          {
+            name: "Volume",
+            data: this.advanced_data,
+          }
+        ],
+        chart: {
+          toolbar: {
+            show: false,
+          },  
+          id: "advanced",
+          group: "stock",
+          type: "area",
+          height: 250
+        },
+        dataLabels: {
+          enabled: false
+        },
+        xaxis: {
+          categories: this.x_axis_data
+        },
+        yaxis: {
+          forceNiceScale: true,
+          labels: {
+            minWidth: 60,
+            maxWidth: 60
+          }
+        },
+        stroke: {
+          curve: "straight",
+          width: [4, 4, 4, 4, 4, 4],
+        }
+      };  
     }
-
-    this.chart2Options.series = data_chart2;
+    
   }
 
 
-  protected updateStockType() {
-    console.log("Graph Type: " + this.selectedGraphType)
-    console.log("Stock Name: " + this.selectedStockType)
+  protected updateBothCharts() {
 
-    var data_chart1 = this.changeChart1()
-    // do chart 2
+    console.log("New Stock Data");
 
-    this.chart1Options.series = [{
-      data: data_chart1,
-      type: this.selectedGraphType
-    }];
-
-    this.chart2Options.series = [{
-      data: this.advanced_data,
-      type: "area"
-    }]
-  }
-
-  protected updateGraphType() {
-    console.log("Graph Type: " + this.selectedGraphType)
-    console.log("Stock Name: " + this.selectedStockType)
-
-    this.chart1Options.series = [{
-      data: this.stock_data, 
-      type: this.selectedGraphType
-    }, {
-      data: [20, 43, 46, 51, 49, 67, 59, 97, 138], 
-      type: "line"
-    }];
+    this.changeChart1();
+    this.changeChart2();
   }
 
 }
