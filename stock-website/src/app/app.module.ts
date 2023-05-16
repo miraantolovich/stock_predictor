8948ee4d048f57369c2eb72efa7980388a7e27a7
Mira Antolovich <48299535+mla4783@users.noreply.github.com>
2023-05-16 09:59:04 -0400

    reverse

--- stock-website/src/app/app.module.ts
+++ stock-website/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { FormsModule } from '@angular/forms'; // Import the FormsModule
 import { AppComponent } from './app.component';
 import { NgApexchartsModule } from "ng-apexcharts";
 import { StockChartComponent } from './stock-chart/stock-chart.component';
-import { CheckboxComponent } from './checkbox/checkbox.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     StockChartComponent,
-    CheckboxComponent,
   ],
   imports: [
     BrowserModule,
